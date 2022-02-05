74e4808a5f29a807961d20eeb8470628a2dfb176
LeenaShree-28 <leenashree28@gmail.com>
2022-02-05 19:58:38 +0530

    Q36 added

--- Feb-05-22/Q36PositiveOrNegative.java
+++ Feb-05-22/Q36PositiveOrNegative.java
@@ -1,7 +1,14 @@
 package basic75programs;
 
+import java.io.*;
+
 public class Q36PositiveOrNegative {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args)throws Exception {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        System.out.println("Enter any number : ");
+        int number = Integer.parseInt(br.readLine());
+        if(number>=0) System.out.println(number+" is Positive.");
+        else System.out.println(number+" is Negative.");
     }
 }
