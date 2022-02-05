b3b7c8af3382d21cdb53270147544a22ac6f5647
LeenaShree-28 <98878569+LeenaShree-28@users.noreply.github.com>
2022-02-05 19:59:32 +0530

    Update Q36PositiveOrNegative.java

--- Feb-05-22/Q36PositiveOrNegative.java
+++ Feb-05-22/Q36PositiveOrNegative.java
@@ -8,6 +8,7 @@ public class Q36PositiveOrNegative {
         BufferedReader br = new BufferedReader(isr);
         System.out.println("Enter any number : ");
         int number = Integer.parseInt(br.readLine());
+        //
         if(number>=0) System.out.println(number+" is Positive.");
         else System.out.println(number+" is Negative.");
     }
