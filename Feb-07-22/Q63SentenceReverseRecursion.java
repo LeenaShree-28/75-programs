527eb280d70023c819e5698038db7b48660bf703
LeenaShree-28 <leenashree28@gmail.com>
2022-02-08 15:45:07 +0530

    changes updated

--- Feb-07-22/Q63SentenceReverseRecursion.java
+++ Feb-07-22/Q63SentenceReverseRecursion.java
@@ -1,7 +1,7 @@
 public class Q63SentenceReverseRecursion {
     public static void main(String[] args) {
         String sentence = "I am Leena Shree";
-        String[] words = sentence.split("\\s");
+        String[] words = sentence.split(" ");
         for (int i = words.length - 1; i >= 0; i--) {
             System.out.print(words[i] + " ");
         }
